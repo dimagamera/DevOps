0194d3435aea6bcc62c5a4faabb99346d22161e6
dimagamera <46674070+dimagamera@users.noreply.github.com>
2020-08-25 17:08:58 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,17 +1,33 @@
 import requests
-URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
 
 
-def get_data(URL):
-    responce = requests.get(URL)
-    data = responce.json()
-    show_currencies(data)
+class privat:
+    def __init__(self):
+        URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+        responce = requests.get(URL)
+        data = responce.json()
+        self.show_currencies(data)
+
+    def show_currencies(self, data):
+        for item in data:
+            print(item["ccy"] + " " + item["base_ccy"] +
+                  " " + item["buy"] + " | " + item["sale"])
 
 
-def show_currencies(currency):
-    for item in currency:
-        print(item["ccy"] + " " + item["base_ccy"] +
-              " " + item["buy"] + " | " + item["sale"])
+class weather:
+    def __init__(self):
+        URL = "https://samples.openweathermap.org/data/2.5/find?q=Dubno&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02"
+        responce = requests.get(URL)
+        data = responce.json()
+        temp = data['list']
+        for item in temp:
+            min_t = item['main']['temp_min']
+            max_t = item['main']['temp_max']
+        print("Мінімальна температура: ", min_t,
+              "\nМаксимальна температура:", max_t)
 
 
-get_data(URL)
+if __name__ == "__main__":
+    a = privat()
+    a.show_currencies('')
+    weather()
